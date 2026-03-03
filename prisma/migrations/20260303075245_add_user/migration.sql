a44c881262b98d3cab71303b2a631d24f9eae289
emonpappu17 <emon.pappu.5@gmail.com>
2026-03-03 14:06:57 +0600

    add: checkauth function

---
+++ prisma/migrations/20260303075245_add_user/migration.sql
@@ -0,0 +1,43 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'HOST', 'ADMIN', 'SUPER_ADMIN');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'SUSPENDED', 'BLOCKED', 'INACTIVE');
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatar" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -2,7 +2,7 @@ import type { Request, Response, NextFunction } from "express";
 import { verifyAccessToken } from "../utils/jwt";
 import { AppError } from "../errors/AppError";
 import { prisma } from "../lib/prisma";
-// import prisma from "../lib/prisma"; // your prisma client
+import type { UserRole } from "../../generated/prisma/enums";
 
 export interface AuthRequest extends Request {
     user?: {
@@ -13,21 +13,27 @@ export interface AuthRequest extends Request {
 }
 
 export const checkAuth =
-    (...allowedRoles: string[]) =>
+    (...allowedRoles: UserRole[]) =>
         async (req: AuthRequest, res: Response, next: NextFunction) => {
             try {
-                const authorization = req.headers.authorization;
+                // const authorization = req.headers.authorization;
 
-                if (!authorization || !authorization.startsWith("Bearer ")) {
+                // if (!authorization || !authorization.startsWith("Bearer ")) {
+                //     throw new AppError("Unauthorized access", 401);
+                // }
+
+                // const token = authorization.split(" ")[1];
+
+                const token = req?.headers.authorization || req?.cookies.accessToken;
+
+                if (!token) {
                     throw new AppError("Unauthorized access", 401);
                 }
 
-                const token = authorization.split(" ")[1];
-
-                // ✅ Verify JWT
+                // Verify JWT
                 const decoded = verifyAccessToken(token as string);
 
-                // ✅ Check if user exists
+                // Check if user exists
                 const user = await prisma.user.findUnique({
                     where: { id: decoded.userId },
                 });
@@ -36,17 +42,17 @@ export const checkAuth =
                     throw new AppError("User does not exist", 404);
                 }
 
-                // ✅ Check if deleted
+                // Check if deleted
                 if (user.isDeleted) {
                     throw new AppError("This account has been deleted", 403);
                 }
 
-                // ✅ Check if blocked
+                // Check if blocked
                 if (user.status === "BLOCKED") {
                     throw new AppError("This account has been blocked", 403);
                 }
 
-                // ✅ Role check
+                // Role check
                 if (allowedRoles.length && !allowedRoles.includes(user.role)) {
                     throw new AppError("Forbidden access", 403);
                 }
@@ -54,6 +60,7 @@ export const checkAuth =
                 // Attach user to request
                 req.user = {
                     userId: user.id,
+                    email: user.email,
                     role: user.role,
                 };
 
