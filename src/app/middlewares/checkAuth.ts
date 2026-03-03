a762085e3e147b5aa79d070e9405bc7854937393
emonpappu17 <emon.pappu.5@gmail.com>
2026-03-03 13:50:18 +0600

    add: initial user schema

---
+++ src/app/middlewares/checkAuth.ts
@@ -0,0 +1,64 @@
+import type { Request, Response, NextFunction } from "express";
+import { verifyAccessToken } from "../utils/jwt";
+import { AppError } from "../errors/AppError";
+import { prisma } from "../lib/prisma";
+// import prisma from "../lib/prisma"; // your prisma client
+
+export interface AuthRequest extends Request {
+    user?: {
+        userId: string;
+        email: string
+        role: string;
+    };
+}
+
+export const checkAuth =
+    (...allowedRoles: string[]) =>
+        async (req: AuthRequest, res: Response, next: NextFunction) => {
+            try {
+                const authorization = req.headers.authorization;
+
+                if (!authorization || !authorization.startsWith("Bearer ")) {
+                    throw new AppError("Unauthorized access", 401);
+                }
+
+                const token = authorization.split(" ")[1];
+
+                // ✅ Verify JWT
+                const decoded = verifyAccessToken(token as string);
+
+                // ✅ Check if user exists
+                const user = await prisma.user.findUnique({
+                    where: { id: decoded.userId },
+                });
+
+                if (!user) {
+                    throw new AppError("User does not exist", 404);
+                }
+
+                // ✅ Check if deleted
+                if (user.isDeleted) {
+                    throw new AppError("This account has been deleted", 403);
+                }
+
+                // ✅ Check if blocked
+                if (user.status === "BLOCKED") {
+                    throw new AppError("This account has been blocked", 403);
+                }
+
+                // ✅ Role check
+                if (allowedRoles.length && !allowedRoles.includes(user.role)) {
+                    throw new AppError("Forbidden access", 403);
+                }
+
+                // Attach user to request
+                req.user = {
+                    userId: user.id,
+                    role: user.role,
+                };
+
+                next();
+            } catch (error) {
+                next(error);
+            }
+        };
