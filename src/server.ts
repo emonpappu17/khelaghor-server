ed43adaea4756ef74dc4131d9ee5537ae29de00f
emonpappu17 <emon.pappu.5@gmail.com>
2026-03-05 12:08:23 +0600

    prepare for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import app from "./app.js";
 import { env } from "./app/config/env.js";
+import { seedSuperAdmin } from "./app/utils/seedAdmin.js";
 
 const PORT = env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await seedSuperAdmin()
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
