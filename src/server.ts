670096ceca790b8788d3d3d5c2eeba0306145ff4
emonpappu17 <emon.pappu.5@gmail.com>
2026-03-05 21:13:38 +0600

    add: deploy fix

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import app from "./app.js";
-import { env } from "./app/config/env.js";
-import { seedSuperAdmin } from "./app/utils/seedAdmin.js";
+// import app from "./app.js";
+// import { env } from "./app/config/env.js";
+// import { seedSuperAdmin } from "./app/utils/seedAdmin.js";
+
+import app from "./app";
+import { env } from "./app/config/env";
+import { seedSuperAdmin } from "./app/utils/seedAdmin";
 
 const PORT = env.PORT;
 
