ef875586510f3e015189e29d21c859a60f15454a
emonpappu17 <emon.pappu.5@gmail.com>
2026-03-06 22:32:24 +0600

    add: host schama

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,3 @@
-// import app from "./app";
-// import { env } from "./app/config/env";
-// import { seedSuperAdmin } from "./app/utils/seedAdmin";
-
-// const PORT = env.PORT;
-
-// app.listen(PORT, async () => {
-//   await seedSuperAdmin()
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-// });
-
-
 import { Server } from 'http';
 import app from "./app";
 import { env } from "./app/config/env";
